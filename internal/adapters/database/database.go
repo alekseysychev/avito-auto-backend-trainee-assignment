16f2cd2fce1f122382147ec79dcad8d24b958e27
Aleksey Sychev <79252045491@yandex.ru>
2021-03-23 04:29:27 +0300

    tests

--- internal/adapters/database/database.go
+++ internal/adapters/database/database.go
@@ -1,8 +1,6 @@
 package database
 
 import (
-	"context"
-
 	"github.com/alekseysychev/avito-auto-backend-trainee-assignment/internal/domain/entities"
 	_ "github.com/jackc/pgx/stdlib"
 	"github.com/jmoiron/sqlx"
@@ -17,8 +15,11 @@ func NewPgEventStorage(dsn string) (*PgLinkStorage, error) {
 	if err != nil {
 		return nil, err
 	}
+	return check(db)
+}
 
-	err = db.Ping()
+func check(db *sqlx.DB) (*PgLinkStorage, error) {
+	err := db.Ping()
 	if err != nil {
 		return nil, err
 	}
@@ -28,14 +29,9 @@ func NewPgEventStorage(dsn string) (*PgLinkStorage, error) {
 func (pges *PgLinkStorage) GetLinkByFrom(from string) (string, error) {
 	var toLink string
 
-	query := `
-		SELECT toLink
-		FROM links
-		WHERE fromLink = $1
-	`
+	row := pges.db.QueryRow("SELECT toLink FROM links WHERE fromLink = $1", from)
 
-	err := pges.db.GetContext(context.Background(), &toLink, query, from)
-	if err != nil {
+	if err := row.Scan(&toLink); err != nil {
 		return "", err
 	}
 
@@ -43,15 +39,13 @@ func (pges *PgLinkStorage) GetLinkByFrom(from string) (string, error) {
 }
 
 func (pges *PgLinkStorage) SaveLink(link entities.Link) bool {
-	query := `
-		INSERT INTO links (fromlink, tolink) 
-		VALUES ($1, $2)
-		ON CONFLICT DO NOTHING RETURNING true;
-	`
+	row := pges.db.QueryRow("INSERT INTO links (fromlink, tolink) VALUES ($1, $2) ON CONFLICT DO NOTHING RETURNING true;",
+		link.From, link.To)
 
-	row := pges.db.QueryRow(query, link.From, link.To)
 	var success bool
-	row.Scan(&success)
+	if err := row.Scan(&success); err != nil {
+		return false
+	}
 
 	return success
 }
